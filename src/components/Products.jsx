e5696743755aeab30e3f439cb282e20803991790
Francisco J. Casillas <chiccasillas@gmail.com>
2024-05-24 11:25:07 -0600

    Create products array with available products

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -1,4 +1,5 @@
 import Product from "./Product";
+import { products } from "../data/products";
 
 function Product({ products }) {
 	return (
---
+++ src/data/products.js
@@ -0,0 +1,17 @@
+export const products = [
+	{
+		name: "Toothbrush",
+		price: 500,
+		inStock: false,
+	},
+	{
+		name: "Toothpaste",
+		price: 70,
+		inStock: true,
+	},
+	{
+		name: "Floss",
+		price: 90,
+		inStock: true,
+	},
+];
